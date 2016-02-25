674c923f86e230e24c34fbc6c4f703ab25ebb4ad
jason <du>
2016-02-26 01:40:31 +0800

    update

--- server-modules/hello.js
+++ server-modules/hello.js
@@ -10,14 +10,19 @@
 'use strict';
 
 const tool = require('./tool');
+const WechatAPI = require('wechat-api');
+
+const api = new WechatAPI('wxfe3526282e13a2bb', '9ec23e0d5dbf7692d1fde3fdb49b51a7');
 
 let pub = {};
 
 pub.hello = (req, res) => {
   tool.l('It works.');
-  res.send({
-    hello: 'It works.'
-  });
+  api.createTmpQRCode(10000, 1800, function (error, result) {
+    res.send({
+      hello: result
+    });
+  })
 };
 
 module.exports = pub;
